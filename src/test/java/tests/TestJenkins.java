478bb596deb105ca32753faff2fba65f9e19be1f
Alexandr Donskov <mrad@list.ru>
2022-03-08 00:36:06 +0300

    add steps allure, screenshot, video

---
+++ src/test/java/tests/TestJenkins.java
@@ -0,0 +1,132 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import tests.pages.RegistrationPage;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static io.qameta.allure.Allure.step;
+
+
+public class TestJenkins {
+
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    String
+            firstName = "Alex",
+            lastName = "Donskov",
+            email = "alex@mail.com",
+            userNumber = "1231231231",
+            day = "12",
+            month = "July",
+            year = "2001",
+            userGender = "Other",
+            subjects = "Math",
+            hobbies = "Sports",
+            loadPicture = "ava.jpg",
+            address = "Some address 1",
+            state = "NCR",
+            City = "Delhi";
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @Test
+    void successFillTest() {
+
+        step("Open home page", () -> {
+            registrationPage.openPage();
+        });
+
+        step("Enter name", () -> {
+            registrationPage.setFirstName(firstName);
+        });
+
+        step("Enter lastname", () -> {
+            registrationPage.setLastName(lastName);
+        });
+
+        step("Enter email", () -> {
+            registrationPage.setUserEmail(email);
+        });
+
+        step("Enter user number", () -> {
+            registrationPage.setUserNumber(userNumber);
+        });
+
+        step("Select gender", () -> {
+            registrationPage.setGender(userGender);
+        });
+
+        step("Select  birthdate", () -> {
+            registrationPage.setBirthDate(day, month, year);
+        });
+
+        step("Enter subjects", () -> {
+            registrationPage.setSubjects(subjects);
+        });
+
+        step("Enter hobbies", () -> {
+            registrationPage.setHobbies(hobbies);
+        });
+
+        step("Load picture", () -> {
+            registrationPage.loadPicture(loadPicture);
+        });
+
+        step("Enter address", () -> {
+            registrationPage.setAddress(address);
+        });
+
+        step("Select state", () -> {
+            registrationPage.setState(state);
+        });
+
+        step("Select city", () -> {
+            registrationPage.setCity(City);
+        });
+
+        step("submit click", () -> {
+            registrationPage.submitClick();
+        });
+
+
+        step("Form check", () -> {
+            registrationPage
+                    .checkForm("Student Name", firstName + " " + lastName)
+                    .checkForm("Student Email", email)
+                    .checkForm("Gender", userGender)
+                    .checkForm("Mobile", userNumber)
+                    .checkForm("Date of Birth", day + " " + month + "," + year)
+                    .checkForm("Subjects", subjects)
+                    .checkForm("Hobbies", hobbies)
+                    .checkForm("Picture", loadPicture)
+                    .checkForm("Address", address)
+                    .checkForm("State and City", state + " " + City);
+        });
+
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/tests/pages/RegistrationPage.java
@@ -0,0 +1,124 @@
+package tests.pages;
+
+
+import com.codeborne.selenide.SelenideElement;
+import tests.pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage<userMailInput> {
+    // components
+    private CalendarComponent calendarComponent = new CalendarComponent();
+
+    // locators
+    private SelenideElement
+            headerTitle = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            resultsTable = $(".table-responsive"),
+            userMailInput = $("#userEmail"),
+            userNumberInput =  $("#userNumber"),
+            userGenderClikc = $("#genterWrapper"),
+            userjectsInput = $("#subjectsInput"),
+            userHobbies = $("#hobbiesWrapper"),
+            loadPicture = $("#uploadPicture"),
+            useraddress =  $("#currentAddress"),
+            setState = $("#react-select-3-input"),
+            setCity = $("#react-select-4-input"),
+            submitClick = $("#submit"),
+            calendar =  $("#dateOfBirthInput");
+
+    // actions
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        headerTitle.shouldHave(text("Student Registration Form"));
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String firstName) {
+        firstNameInput.setValue(firstName);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+
+        return this;
+    }
+
+    public RegistrationPage setUserEmail(String email) {
+        userMailInput.setValue(email);
+        return this;
+    }
+
+
+    public RegistrationPage setUserNumber (String number) {
+        userNumberInput.setValue(number);
+        return this;
+    }
+
+
+    public RegistrationPage setSubjects(String subjects) {
+        userjectsInput.setValue(subjects).pressEnter();
+        return this;
+
+    }
+
+
+    public RegistrationPage setGender (String userGender) {
+        userGenderClikc.$(byText(userGender)).click();
+        return this;
+    }
+
+    public RegistrationPage setHobbies (String hobbies) {
+        userHobbies.$(byText(hobbies)).click();
+        return this;
+    }
+
+    public RegistrationPage loadPicture(String name) {
+        loadPicture.uploadFromClasspath("ava.jpg");
+        return this;
+    }
+
+
+
+    public  RegistrationPage setAddress (String address){
+        useraddress.setValue(address);
+        return this;
+    }
+
+    public RegistrationPage setState(String state) {
+        setState.setValue(state).pressEnter();
+        return this;
+    }
+
+    public RegistrationPage setCity(String City) {
+        setCity.setValue(City).pressEnter();
+        return this;
+    }
+
+    public RegistrationPage submitClick() {
+        submitClick.click();
+        return this;
+    }
+
+    public RegistrationPage setBirthDate(String day, String month, String year) {
+        calendar.click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+
+
+
+    public RegistrationPage checkForm(String fieldName, String value) {
+        resultsTable.$(byText(fieldName))
+                .parent().shouldHave(text(value));
+        return this;
+    }
+
+}
